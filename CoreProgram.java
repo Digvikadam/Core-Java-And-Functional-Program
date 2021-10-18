9af4692a513afbe9f23f1efd1e38359afe102dca
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-18 11:39:32 +0530

    Added: Largest_Number

--- CoreProgram.java
+++ CoreProgram.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 public class CoreProgram {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a Charactor:");
-		char ch = sc.next().charAt(0);
-		if (ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u') {
-			System.out.println("Given charactor is Vowel");
+		System.out.println("Enter First Number:");
+		int a = sc.nextInt();
+		System.out.println("Enter Second Number:");
+		int b = sc.nextInt();
+		System.out.println("Enter Third Number");
+		int c = sc.nextInt();
+		if(a>=b && a>=c) {
+			System.out.println("The Largest Number is:"+a);
+		}
+		else if(b>=a && b>=c) {
+			System.out.println("The Largest Number is:"+b);
 		}
 		else {
-			System.out.println("Given charactor is Consonant");
-		}
+			System.out.println("The Largest Number is:"+c);
+		}	
 	}
 }
