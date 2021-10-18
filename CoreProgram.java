65e60f2c5f7937a37d7705df81e5ab45c4db2694
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-18 11:34:06 +0530

    Added: Alphabet_Vowel_Consonant

--- CoreProgram.java
+++ CoreProgram.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class CoreProgram {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Number:");
-		int num = sc.nextInt();
-		if(num % 2 ==0) {
-			System.out.println("Number is Even");
+		System.out.println("Enter a Charactor:");
+		char ch = sc.next().charAt(0);
+		if (ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u') {
+			System.out.println("Given charactor is Vowel");
 		}
 		else {
-			System.out.println("Number is Odd");
+			System.out.println("Given charactor is Consonant");
 		}
 	}
 }
